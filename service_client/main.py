49740e132936ee3181f50830c3ee9092f0c68646
Marvin <32267352+dermrvn-code@users.noreply.github.com>
2024-12-21 10:28:21 +0100

    Add file upload functionality and download certificate feature

--- service_client/main.py
+++ service_client/main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from flask import Flask, request, render_template
+from flask import Flask, redirect, request, render_template
 import ssl
 
 # Add parent directory to path
@@ -9,6 +9,7 @@ parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
 sys.path.append(parent_dir)
 
 from functions import generate_certificate
+from certificates.Certificate import load_certificate_bytes_from_path
 
 app = Flask(__name__)
 
@@ -18,8 +19,26 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/upload_request", methods=["GET", "POST"])
+@app.route("/upload_file", methods=["GET", "POST"])
 def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        matriculation_number = request.form["matriculation_number"]
+        file_name = request.form["file_name"]
+        if file:
+            upload_folder = os.path.join(script_dir, "uploads")
+            os.makedirs(upload_folder, exist_ok=True)
+            new_file_name = f"{matriculation_number}_{file_name}"
+            file_ext = os.path.splitext(file.filename)[1]
+            new_file_name += file_ext
+            file_path = os.path.join(upload_folder, new_file_name)
+            file.save(file_path)
+            return redirect("../")
+    return render_template("upload_file.html")
+
+
+@app.route("/upload_request", methods=["GET", "POST"])
+def upload_request():
     if request.method == "POST":
         file = request.files["file"]
         if file:
@@ -34,6 +53,22 @@ def upload_file():
             )
             return response
     return render_template("upload_request.html")
+
+
+@app.route("/download_cert", methods=["GET"])
+def download_cert():
+    cert_bytes = load_certificate_bytes_from_path(
+        os.path.join(script_dir, "certs", "cert_campusofficehshl.crt")
+    )
+    response = app.response_class(
+        response=cert_bytes,
+        status=200,
+        mimetype="application/x-x509-ca-cert",
+        headers={
+            "Content-Disposition": "attachment; filename=COCertificate.crt",
+        },
+    )
+    return response
 
 
 if __name__ == "__main__":
